9b710ff4154d9a3c658e1a80698eb1fe06d8699f
lijunaa <lijunaa@yonyou.com>
2019-01-21 22:12:17 +0800

    语法糖

---
+++ es6.js
@@ -0,0 +1,42 @@
+
+//var  重复声明 不能限制修改 函数级
+
+//let  不能重复声明 块级
+
+//函数
+
+let arr=[12,34,4545,676]
+
+arr.sort((n1,n2)=>n1-n2)
+
+console.log(arr)
+
+//数组展开
+let arr1=[1,2,4];
+let arr2=[3,45,34];
+
+arr=[...arr1,...arr2];
+
+//arr=arr1.concat(arr2)
+
+console.log(arr)
+
+
+//数组球平均数
+
+
+let arr3=arr1.reduce((first,scend,index)=>{
+    if(index==arr1.length-1){
+        return (first+scend)/arr1.length
+    }else{
+        return first+scend
+    }
+
+})
+
+console.log(arr3)
+
+
+arr.forEach((item,index)=>{
+    console.log(`第${index}个：${item}`)
+})
