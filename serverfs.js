b877775a7bafa13365ff2bcbfde21e849e707942
lijunaa <lijunaa@yonyou.com>
2019-01-22 22:19:32 +0800

    数据解析

--- serverfs.js
+++ serverfs.js
@@ -1,9 +1,13 @@
 const http=require('http');
 const fs=require('fs');
+const querystring=require('querystring');//解析get数据
+const urlLib=require('url');
 
 http.createServer(function(req,res){
 
     var  file_name='./www'+req.url;
+    var  urlPars=urlLib.parse('http://www.baidu.com/index?a=3&b=6',true);
+    console.log(urlPars.pathname,urlPars.query)
 
     fs.readFile(file_name,function(err,data){
         if(err){
