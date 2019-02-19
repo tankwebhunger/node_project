e17eea86daac6d1f3c58ae4dd89aeb5b5a526861
lijunaa <lijunaa@yonyou.com>
2019-02-19 09:38:49 +0800

    前端算法

--- fs.js
+++ fs.js
@@ -12,7 +12,7 @@ fs.writeFile('bbb.txt','asdasdads',function(err){
 //huoqu
 function GetRequest() {  
     var url = location.search; //获取url中"?"符后的字串  
-    var theRequest = new Object();  
+    var theRequest = {};  
     if (url.indexOf("?") != -1) {  
        var str = url.substr(1);  
        strs = str.split("&");  
@@ -22,3 +22,41 @@ function GetRequest() {
     }  
     return theRequest;  
  }  
+
+ //冒泡排序
+
+ function bubblesort(arr){
+     let j=arr.length;
+     while(j>0){
+         let p=0;
+         for(let i=0;i<j;i++){
+             if(arr[i]>arr[i+1]){
+                 p=i
+                 let tem=arr[i];
+                 arr[i]=arr[i+1];
+                 arr[i+1]=tem
+             }
+         }
+         i=p
+     }
+
+     return arr
+ }
+ //二分法查找
+
+ function middleSearch(arr,data){
+
+    var low=0;
+    var hight=arr.length-1;
+    while(low<=hight){
+        var middle=Math.floor((low+hight)/2)
+        if(arr[middle]<data){
+            low=middle+1
+        }else if(arr[middle]>data){
+            hight=middle-1
+        }else{
+            return middle
+        }
+    }
+    return -1
+ }
