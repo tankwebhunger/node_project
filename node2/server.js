385e6b50a9f2fefbcc81d49003075c4b189c011c
lijunaa <lijunaa@yonyou.com>
2019-03-10 23:04:15 +0800

    添加备注

--- node2/server.js
+++ node2/server.js
@@ -2,5 +2,16 @@
 //express 中间件
 
 //get 无需中间件 req.query 
-//链式操作
+//链式操作 next()
 //post 需要中间件 body-parser req.body
+
+
+//----------
+//cookie  再浏览器保存数据，每次请求都会发送到服务器 不安全
+
+//cookie空间非常小，省的用 安全性非常差
+
+
+//session 保存数据，保存在服务端 基于cookie来实现的 
+
+//cookie当中会有一个session_id 有session劫持危险
